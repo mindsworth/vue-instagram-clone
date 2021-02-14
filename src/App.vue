533d12296923e6a8443f9711e513422fde459f0d
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-02-14 04:02:23 +0100

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="view">
+    <Header />
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view />
   </div>
-  <router-view />
 </template>
 
+<script>
+import Header from "./components/layout/Header";
+
+export default {
+  name: "App",
+  components: { Header }
+};
+</script>
+
 <style lang="scss">
+body,
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.view {
+  min-height: 100vh;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  margin: 0 auto;
+  max-width: 960px;
+  height: 100%;
+  padding: 0 20px;
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="header">
+    <div class="container">
+      <img
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+        alt="Instagram logo"
+        class="img-fluid"
+      />
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.header {
+  height: 54px;
+  border-bottom: 1px solid rgba(219, 219, 219, 1);
+}
+
+.header .container {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.img-fluid {
+  max-height: 100%;
+  max-width: 100%;
+  object-fit: contain;
+}
+</style>
